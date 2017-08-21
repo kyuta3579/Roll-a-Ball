7c14477a64779a2716e0cb7b240da247ca623f06
kyuta3579 <kyuta3579@yahoo.co.jp>
2017-08-21 21:22:29 +0900

    commit

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerWall : MonoBehaviour {
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+
+        }
+        
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+http://tutorial.unity3d.jp/
+にあるUnityの3D玉転がしチュートリアルです。
